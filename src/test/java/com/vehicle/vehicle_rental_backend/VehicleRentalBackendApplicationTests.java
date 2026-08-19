7d741ba93a5728821b07d0d518d2c9c764e01466
Mounika <mounika2004117@gmail.com>
2026-08-19 17:10:02 +0530

    Initial Spring Boot backend setup

---
+++ src/test/java/com/vehicle/vehicle_rental_backend/VehicleRentalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vehicle.vehicle_rental_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleRentalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
